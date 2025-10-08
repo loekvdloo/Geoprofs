428193f7d8a29ebb8d1928607146f06744b445e4
aatje-sy <ahas.alasmi@gmail.com>
2025-10-08 12:43:52 +0200

    Verlofaanvragen models

--- app/Models/Verloftype.php
+++ app/Models/Verloftype.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Verloftype extends Model
-{
-    //
+class Verloftype extends Model {
+    protected $table = 'verloftype';
+    protected $primaryKey = 'verlof_type_id';
+    public $timestamps = false;
+    protected $fillable = ['naam','betaald'];
 }
