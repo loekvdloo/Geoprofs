32042717e550311be2ba2dfde0d9d521b8c7917f
aatje-sy <ahas.alasmi@gmail.com>
2025-10-08 11:12:05 +0200

    ERD -> Migrations ✅

---
+++ database/migrations/2025_10_08_082009_create_verlofaanvraags_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('verlofaanvraag', function (Blueprint $table) {
+            $table->id('aanvraag_id');
+
+
+            $table->foreignId('medewerker_id')->constrained('users')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('verlof_type_id');
+            $table->foreign('verlof_type_id')
+                ->references('verlof_type_id')
+                ->on('verloftype')
+                ->restrictOnDelete();
+
+            $table->date('start_datum');
+            $table->date('eind_datum');
+            $table->text('reden');
+            $table->dateTime('aanvraag_datum');
+            $table->string('status')->default('pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('verlofaanvraags');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VerlofaanvraagController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/verlof/aanvragen/nieuw', [VerlofaanvraagController::class, 'create'])->name('verlof.create');
+    Route::post('/verlof/aanvragen', [VerlofaanvraagController::class, 'store'])->name('verlof.store');
 });
 
 require __DIR__.'/auth.php';
