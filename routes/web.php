9cc8164e477a52974c0044d707de9fc327436c14
Andypjee <153715999+Andypjee@users.noreply.github.com>
2025-10-28 11:14:13 +0100

    hernoemen van test.jsx naar aanvraag.jsx

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/verlof/test', function () {
-        return Inertia::render('Verlof/Test', [
+    Route::get('/verlof/aanvraag', function () {
+        return Inertia::render('Verlof/aanvraag', [
             'types' => Verloftype::orderBy('naam')->get(['verlof_type_id','naam','betaald']),
         ]);
     });    Route::post('/verlof/aanvragen', [VerlofaanvraagController::class, 'store'])->name('verlof.store');
