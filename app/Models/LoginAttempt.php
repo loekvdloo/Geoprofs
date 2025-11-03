c6e5840ab0454ed68e117d52fe4604b7edd317b6
aatje-sy <ahas.alasmi@gmail.com>
2025-11-03 11:11:44 +0100

    EXTRA COMMIT

---
+++ app/Models/LoginAttempt.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LoginAttempt extends Model
+{
+    protected $table = 'login_attempts';
+    protected $primaryKey = 'attempt_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'user_id',
+        'attempt_time',
+        'attempt_ip',
+        'succes',
+        'failure_reason',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+}
